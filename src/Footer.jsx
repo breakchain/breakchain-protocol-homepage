0c29c7ec200e678d0f0cc0a16351c003d3ac8e11
Garett Jaeckel <eternal1@eternalpro.com>
2022-04-10 09:55:33 -0500

    create logos, create footer, cleanup of files

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,11 @@
 import React, {Component} from 'react'
 import logo_aws from './assets/images/logos_sm/aws_logo_smile.jpg'
-
+import logo_ethereum from './assets/images/logos_sm/ethereum-logo-portrait-black-gray.jpg'
+import logo_github from './assets/images/logos_sm/github.jpg'
+import logo_heroku from './assets/images/logos_sm/heroku-vector-logo.jpg'
+import logo_metamask from './assets/images/logos_sm/metamask.jpg'
+import logo_polygon from './assets/images/logos_sm/polygon-matic5119.jpg'
+import logo_ud from './assets/images/logos_sm/Unstoppable_Domains.jpg'
 
 class Footer extends Component {
   render() {
@@ -15,32 +20,32 @@ class Footer extends Component {
                 </div>
                 <div class="partner">
                     <a href="#">
-                        <img src="./assets/images/logos_sm/ethereum-logo-portrait-black-gray.jpg" alt="AWS" />
+                        <img src={logo_ethereum} alt="ethereum" />
                     </a>
                 </div>
                 <div class="partner">
                     <a href="#">
-                        <img src="./assets/images/logos_sm/github.jpg" alt="AWS" />
+                        <img src={logo_github} alt="Github" />
                     </a>
                 </div>
                 <div class="partner">
                     <a href="#">
-                        <img src="./assets/images/logos_sm/heroku-vector-logo.jpg" alt="AWS" />
+                        <img src={logo_heroku} alt="heroku" />
                     </a>
                 </div>
                 <div class="partner">
                     <a href="#">
-                        <img src="./assets/images/logos_sm/metamask.jpg" alt="AWS" />
+                        <img src={logo_metamask} alt="Metamask" />
                     </a>
                 </div>
                 <div class="partner">
                     <a href="#">
-                        <img src="./assets/images/logos_sm/polygon-matic5119.jpg" alt="AWS" />
+                        <img src={logo_polygon} alt="Polygon" />
                     </a>
                 </div>
                 <div class="partner">
                     <a href="#">
-                        <img src="./assets/images/logos_sm/Unstoppable_Domains.jpg" alt="AWS" />
+                        <img src={logo_ud} alt="Unstoppable_Domains" />
                     </a>
                 </div>
             </div>
